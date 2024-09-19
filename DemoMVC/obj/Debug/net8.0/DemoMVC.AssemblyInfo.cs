64a4be62c951ff4002a624bd16704201d3c1f16f
ngoc0102 <ngoc0102@gmail.com>
2024-09-19 09:45:40 +0700

    commit note

--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ac02373e36b1e40e61be69480c07566f1aaa1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8400723614e8a495a1591c20572bec9d1bf4fb54")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
