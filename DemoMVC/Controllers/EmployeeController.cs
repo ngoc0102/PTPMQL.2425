c70480e9e038ee6257fc3689c9a97fc40003b78e
ngoc0102 <ngoc0102@gmail.com>
2024-09-24 15:18:22 +0700

    commit note

--- DemoMVC/Controllers/EmployeeController.cs
+++ DemoMVC/Controllers/EmployeeController.cs
@@ -20,6 +20,20 @@ namespace DemoMVC.Controllers
         {
             return View();
         }
+    public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Employee = await _context.Employee.FindAsync(id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return View(Employee);
+        }
 
  [HttpPost]
         [ValidateAntiForgeryToken]
@@ -32,8 +46,108 @@ namespace DemoMVC.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(Employee);
+        }[HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("")] Employee Employee)
+        {
+            if (id != Employee.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!personExists(Employee.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
         }
 
+        // GET: person/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Employee = await _context.Employee
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(Employee);
+        }
+
+ [HttpPost, ActionName("Delete")]
+     [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var Employee = await _context.Employee.FindAsync(id);
+            if (Employee != null)
+            {
+                _context.Employee.Remove(Employee);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool personExists(int id)
+        {
+            return _context.Employee.Any(e => e.ID == id);
+        }
+    public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Employee = await _context.Employee
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(Employee);
+        }
+
+ [HttpPost, ActionName("Details")]
+     [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DetailsConfirmed(int id)
+        {
+            var Employee = await _context.Employee.FindAsync(id);
+            if (Employee != null)
+            {
+                _context.Employee.Remove(Employee);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employee.Any(e => e.ID == id);
+        }
     }
 }
-        
--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69e51dbe1ab3cacd04343cd8f7130a261c4a82b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dabef81136e8bada3f1b1d181915514f674de9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
